78612b6480f6d3b3aebd91d766eb9df0d0712254
Bartek Taczała <bartek.taczala@gmail.com>
2015-06-26 13:52:08 +0200

    Some work on Rule_15_5_2

--- src/rules/Rule_15_5_2.cpp
+++ src/rules/Rule_15_5_2.cpp
@@ -13,6 +13,19 @@
 
 using namespace clang;
 
+namespace {
+  std::vector<QualType> getExceptionSpecification(FunctionDecl *F) {
+    std::vector<QualType> ret;
+    auto fun = F->getType()->getAs<FunctionProtoType>();
+    if (fun) {
+      for(std::size_t i = 0;i<fun->getNumExceptions();++i) {
+        ret.emplace_back(fun->getExceptionType(i));
+      }
+    }
+    return ret;
+  }
+}
+
 namespace misracpp2008 {
 
 class Rule_15_5_2 : public RuleCheckerASTContext,
@@ -20,20 +33,40 @@ class Rule_15_5_2 : public RuleCheckerASTContext,
 public:
   Rule_15_5_2() : RuleCheckerASTContext() {}
 
-  //bool VisitFunctionDecl(FunctionDecl *F) {
-    //if (doIgnore(F->getLocStart())) {
-      //return true;
-    //}
-    //F->dumpColor();
-    //return true;
-  //}
-
   bool VisitDecl(Decl *D) {
     if (doIgnore(D->getLocStart())) {
       return true;
     }
 
+    //D->dumpColor();
+
     if (isa<FunctionDecl>(D)) {
+    }
+
+    return true;
+  }
+
+  bool VisitFunctionDecl(FunctionDecl *F) {
+    if (doIgnore(F->getLocStart())) {
+      return true;
+    }
+    for(auto decl: F->decls()) {
+      decl->dumpColor();
+    }
+    return true;
+  }
+
+  bool VisitCXXMethodDecl(CXXMethodDecl *M) {
+    if (doIgnore(M->getLocStart())) {
+      return true;
+    }
+
+    std::vector<QualType> exceptions {getExceptionSpecification(M)};
+    M->dumpColor();
+
+    // if not exception specification return 
+    if (exceptions.empty()) {
+      return true;
     }
 
     return true;
@@ -44,6 +77,7 @@ protected:
     RuleCheckerASTContext::doWork();
     this->TraverseDecl(context->getTranslationUnitDecl());
   }
+private:
 };
 
 static RuleCheckerASTContextRegistry::Add<Rule_15_5_2> X("15-5-2", "");
